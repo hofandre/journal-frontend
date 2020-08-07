799a1b75e2e16c3e427105ccb8f210fd1111d4de
Andrew Hoffman <hofandre@umich.edu>
2020-08-07 10:23:01 -0400

    Another push event

--- src/services/entry.service.js
+++ src/services/entry.service.js
@@ -2,6 +2,7 @@ const { default: axios } = require('axios')
 
 class EntryService {
     constructor() {
+        // This probably doesn't work
         this.URI = 'http://localhost:5000/entries'
         // this.URI = 'http://ec2-13-58-65-253.us-east-2.compute.amazonaws.com:5000/entries'
     }
