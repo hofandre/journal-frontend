6a6bf733b2ba98a842f45632c39a156095266455
Andrew Hoffman <hofandre@umich.edu>
2020-08-07 10:21:27 -0400

    Forcing an build in Jenkins

--- src/services/entry.service.js
+++ src/services/entry.service.js
@@ -2,7 +2,6 @@ const { default: axios } = require('axios')
 
 class EntryService {
     constructor() {
-        //ec2-13-58-65-253.us-east-2.compute.amazonaws.com
         this.URI = 'http://localhost:5000/entries'
         // this.URI = 'http://ec2-13-58-65-253.us-east-2.compute.amazonaws.com:5000/entries'
     }
